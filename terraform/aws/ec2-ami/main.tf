7bfd46fa006e63e00c28ec9c9dbaeaf426e8e1cd
Chris Wang <git@chriswang.tech>
2023-11-06 22:43:59 +0800

    updated user_data renamed orginal to allowed

--- terraform/aws/ec2-ami/main.tf
+++ terraform/aws/ec2-ami/main.tf
@@ -24,37 +24,37 @@ variable "prefix" {
 
 }
 
-resource "aws_instance" "original_instance" {
+resource "aws_instance" "allowed_instance" {
   # Ubuntu Server 22.04 LTS (HVM), SSD Volume Type
   ami           = "ami-0fc5d935ebf8bc3bc"
   instance_type = "t2.nano"
   tags = {
-    Name = "${var.prefix}-orignal-builder"
+    Name = "${var.prefix}-allowed-builder"
   }
 
 }
 
-resource "aws_ami_from_instance" "original_ami" {
-  name               = "${var.prefix}-orignal"
-  source_instance_id = aws_instance.original_instance.id
+resource "aws_ami_from_instance" "allowed_ami" {
+  name               = "${var.prefix}-allowed"
+  source_instance_id = aws_instance.allowed_instance.id
 }
 
 
 resource "aws_instance" "malicious_builder_instance" {
-  ami           = aws_ami_from_instance.original_ami.id
+  ami           = aws_ami_from_instance.allowed_ami.id
   instance_type = "t2.nano"
   tags = {
     Name = "${var.prefix}-malicious-builder"
   }
   user_data = <<-EOF
               #!/bin/bash
-              wget https://secure.eicar.org/eicar.com
+              wget -P /root/ https://secure.eicar.org/eicar.com
               EOF  
 }
 
 resource "aws_ami_from_instance" "malicious_ami" {
   name               = "${var.prefix}-malicious"
-  source_instance_id = aws_instance.original_instance.id
+  source_instance_id = aws_instance.malicious_builder_instance.id
 }
 
 resource "aws_instance" "malicious_runner_instance" {
